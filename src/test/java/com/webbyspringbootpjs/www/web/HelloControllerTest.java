d059c8f99f3c85b9b707757dc9c260f87e6ea71a
JeongSeopPark <kbp1021@naver.com>
2022-02-03 16:00:16 +0900

    gitignore 등록

--- src/test/java/com/webbyspringbootpjs/www/web/HelloControllerTest.java
+++ src/test/java/com/webbyspringbootpjs/www/web/HelloControllerTest.java
@@ -4,15 +4,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@MockBean(JpaMetamodelMappingContext.class)
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
+@WebAppConfiguration
 public class HelloControllerTest {
 
     @Autowired
